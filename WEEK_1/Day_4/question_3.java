780def0720fd5ee94709843e7add156380e57e48
Kuljeet-soni <kuljeetsoni9368@gmail.com>
2026-06-04 20:16:01 +0530

    program

---
+++ WEEK_1/Day_4/question_3.java
@@ -0,0 +1,50 @@
+package WEEK_1.Day_4;
+
+import java.util.Scanner;
+
+public class question_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int arr[][] = new int[r][c];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+        Spiral(arr);
+    }
+
+    public static void Spiral(int[][]arr){
+        int minr=0;
+        int maxr=arr.length-1;
+        int minc=0;
+        int maxc=arr[0].length-1;
+        int total_element=arr.length*arr[0].length;
+        int count=0;
+        while(count<total_element){
+            for(int i=minc;i<=maxc&&count<total_element;i++){
+                System.out.print(arr[minr][i]+" ");
+                count++;
+            }
+            minr++;
+            for(int i=minr;i<=maxr&&count<total_element;i++){
+                System.out.print(arr[i][maxc]+" ");
+                count ++;
+            }
+            maxc--;
+             for(int i=maxc;i>=minc&&count<total_element;i--){
+                System.out.print(arr[maxr][i]+" ");
+                count ++;
+        }
+        maxr-- ;
+         for(int i=maxr;i>=minr&&count<total_element;i--){
+                System.out.print(arr[i][minc]+"");
+                count ++;
+    }
+            minc++ ;
+}
+    }
+}
